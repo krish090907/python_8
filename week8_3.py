42210c6c95682e89a27bb110bbcdc57539c8eefe
krish090907 <24331a05d7@mvgrce.edu.in>
2026-03-29 18:11:15 +0530

    Add files via upload

---
+++ week8_3.py
@@ -0,0 +1,22 @@
+student = {
+    "name": "Koushik",
+    "age": 18,
+    "course": "CSE",
+    "year": 2
+}
+
+
+print("Keys:", student.keys())
+print("Values:", student.values())
+
+
+print("Items:", student.items())
+
+
+removed_value = student.pop("year")
+print("After pop():", student)
+print("Popped value:", removed_value)
+
+
+del student["age"]
+print("After delete():", student)
